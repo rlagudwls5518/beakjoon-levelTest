3ffed1f67246930f4af803b62b846a43bfe81b72
rlagudwls5518 <66249379+rlagudwls5518@users.noreply.github.com>
2023-12-27 14:35:22 +0900

    Add files via upload

---
+++ Step 2/14681번.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+//사분면 고르기
+//점 (x, y)의 사분면 번호(1, 2, 3, 4 중 하나)를 출력한다.
+int main()
+{
+	int x, y=0;
+	cin >> x;
+	cin >> y;
+
+	if (x > 0 && y > 0)cout << "1";
+	if (x < 0 && y > 0)cout << "2";
+	if (x < 0 && y < 0)cout << "3";
+	if (x > 0 && y < 0)cout << "4";
+}
+
+
---
+++ Step 2/9498번.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+//시험성적
+//시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+int main()
+{
+	int score = 0;
+	cin >> score;
+
+	if (score >= 90 && score <= 100) cout << "A";
+	else if (score >= 80 && score <= 89) cout << "B";
+	else if (score >= 70 && score <= 79) cout << "C";
+	else if (score >= 60 && score <= 69) cout << "D";
+	else cout << "F";
+}
